29f1177a92d59861de5c91591e94810d380168e1
CassandreV <cassandre.vanzetta@live.fr>
2020-10-29 20:01:03 +0100

    ite 4 done

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import FoodBox from './Components/FoodBox'
 import AddNew from './Components/addNew'
 
 class App extends React.Component {
-  state = { foods: foods ,
-            btnClicked: false
+  state = { 
+    foods: foods,
+    btnClicked: false,
+    value: ''
   }
 
   // showForm =(event)=>{
@@ -23,28 +25,55 @@ class App extends React.Component {
     })
   }
 
+  /*Fonction HandleChange for value*/
+  handleChange = (e) => {
+    this.setState({value: e.target.value});
+  }
+
   render() {
     const btnClicked = this.state.btnClicked
+    let filteredFood = [];
+
     return (
       <div className="App">
         <h1>IronNutrition</h1>
-        <div className="searchBar"><input
+        <div className="searchBar">
+          <input
               type="text"
               class="input search-bar"
               name="search"
               placeholder="Search"
-              value=""
-            />
+              value={this.state.value}
+              onChange={this.handleChange}
+          />
         </div>
         {/* this.setState({}) */}
         <button onClick={() => this.setState({btnClicked: true})}>Add New</button>
         {/*si boutton clicced? && affiche formulaire : */}
         {btnClicked && <AddNew toto={this.addFoodHandler} />}
         
+        {/* Si search 
+              //si value.length > 0
+              condition ? true : false
+                alors affiche ce qui correspond au search
+                  // convertir le mot en tableau
+                  // chercher si le tableau contient la "lettre" recherchée
+                sinon affiche tout */}
 
-        {this.state.foods.map(food => (
+        {this.state.value.length > 0 
+        ?
+         /*Si recherche*/
+         this.state.foods.map(food => (
+          food.name.includes(this.state.value) && <FoodBox name={food.name} calories={food.calories} image={food.image} quantity={food.quantity}></FoodBox>
+          ))
+         : 
+         /*Si pas de recherche*/
+         this.state.foods.map(food => (
           <FoodBox name={food.name} calories={food.calories} image={food.image} quantity={food.quantity}></FoodBox>
-        ))}
+          ))
+        }
+
+
         
       </div>
     );
